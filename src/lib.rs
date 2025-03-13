bf51ab4fac6365ca6411df2425a1605199ad79db
Li Hua <lihua@email.com>
2025-03-13 16:36:14 +0800

    feat: update .gitignore and optimize Rust code
    - Updated .gitignore to include rules for Linux, macOS, Windows, Vim, and Rust.
    - Optimized Rust code by using LazyLock for static initialization.

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,14 @@ use rand::prelude::*;
 use rust_embed::Embed;
 use std::collections::HashSet;
 use std::string::String;
+use std::sync::LazyLock;
+
 #[derive(Embed)]
 #[folder = "千字文"]
 #[include = "*.txt"]
 struct Asset;
 
-fn init() -> Vec<Vec<char>> {
+static QZW: LazyLock<Vec<Vec<char>>> = LazyLock::new(|| {
     let mut result = vec![HashSet::new(); 1000];
     for file in Asset::iter() {
         for (index, item) in std::str::from_utf8(&Asset::get(&file).unwrap().data)
@@ -24,11 +26,11 @@ fn init() -> Vec<Vec<char>> {
         .iter()
         .map(|x| x.iter().cloned().collect())
         .collect::<Vec<Vec<char>>>();
-}
+});
 
 pub fn encode(text: String) -> String {
     let mut rng = rand::rng();
-    let qzw: Vec<Vec<char>> = init();
+    let qzw = &*QZW;
     let mut tmp: String = BigInt::from_bytes_be(Sign::Plus, text.as_bytes()).to_string();
     tmp = "0".repeat((3 - tmp.len() % 3) % 3) + &tmp;
     return tmp
@@ -43,6 +45,6 @@ pub fn encode(text: String) -> String {
 }
 
 pub fn decode(_text: String) {
-    let qzw: Vec<Vec<char>> = init();
+    let qzw = &*QZW;
     dbg!(qzw);
 }
